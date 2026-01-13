19c56cda746b7891249a73cf364efbd472052087
sebcsiz <seb.csiz@gmail.com>
2026-01-13 13:38:37 -0800

    Added age text to hero.tsx

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -122,7 +122,7 @@ export default function Hero() {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.6, delay: 0.6 }}
         >
-          <AgeTicker />
+          Currently<AgeTicker />years old.
         </motion.div>
 
         <motion.div
